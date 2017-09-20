7f83292bf5bb2ba2aa1ff31173868d830c115384
Dhrumil1808 <dhrml.shah@gmail.com>
2017-09-20 16:48:31 -0700

    components with modules

---
+++ Exercise/Ch03/03_04/src/Components/App.js
@@ -0,0 +1,11 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Header from './Header'
+const App=(props) => 
+{
+	return (<div> <Header message="Naming contest" /> </div>);
+};
+
+export default App;
---
+++ Exercise/Ch03/03_04/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './Components/App'
+const color = Math.random() > 0.5 ? 'Green' : 'Red'
+
+
+
+
+
+ReactDOM.render( <App  /> ,document.getElementById('root'));
