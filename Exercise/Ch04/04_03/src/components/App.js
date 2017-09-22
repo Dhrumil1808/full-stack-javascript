6b9981c382bb1e738e7393959e30abba2d38df9a
Dhrumil1808 <dhrml.shah@gmail.com>
2017-09-21 17:24:22 -0700

    Sass with Node

---
+++ Exercise/Ch04/04_03/src/components/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Header from './Header';
+
+import ContestPreview from './ContestPreview';
+
+class App extends React.Component {
+  state = {
+    pageHeader: 'Naming Contests'
+  };
+  componentDidMount() {
+    // timers, listeners
+  }
+  componentWillUnmount() {
+    // clean timers, listeners
+  }
+  render() {
+    return (
+      <div className="App">
+        <Header message={this.state.pageHeader} />
+        <div>
+        {this.props.contests.map(function(contest) {return <ContestPreview contest={contest}  /> }) }
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
