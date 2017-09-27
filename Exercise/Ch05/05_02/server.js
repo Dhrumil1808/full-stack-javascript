d218dfc2dbd220b32d41a75d555fc09a18df8e2f
Dhrumil1808 <dhrml.shah@gmail.com>
2017-09-27 15:40:43 -0700

    ReactDOMServer

---
+++ Exercise/Ch05/05_02/server.js
@@ -0,0 +1,24 @@
+import config from './config';
+import apiRouter from './api';
+import serverRender from './serverRender';
+import express from 'express';
+const server = express();
+
+server.set('view engine', 'ejs');
+
+server.get('/', (req, res) => {
+	serverRender().then(
+		content =>{
+			res.render('index',{
+				content
+			});
+	})
+	.catch(console.error);
+});
+
+server.use('/api', apiRouter);
+server.use(express.static('public'));
+
+server.listen(config.port, config.host, () => {
+  console.info('Express listening on port', config.port);
+});
---
+++ Exercise/Ch05/05_02/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './components/App';
+
+import data from './testData';
+
+ReactDOM.render(
+  <App initialConstests={[]}  />,
+  document.getElementById('root')
+);
+
